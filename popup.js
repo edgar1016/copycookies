a837a0159735c3496cebc8c900d91a47947e0170
KUROSHIORI\edgar <edgar135300@gmail.com>
2023-08-17 03:31:30 -0700

    added userAgent to JSON

--- popup.js
+++ popup.js
@@ -1,18 +1,28 @@
 (async function() {
-	let [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-	chrome.cookies.getAll({"url":tab.url}, cookies => {
-		var area = document.createElement('textarea');
-		area.style.position = 'absolute';
-		area.style.border = '0';
-		area.style.padding = '0';
-		area.style.margin = '0';
-		area.style.height = '1px';
-		area.style.top = '-10px';
-		area.innerText = JSON.stringify(cookies);
-		document.body.appendChild(area, document.body.firstChild);
-		area.focus();
-		area.select();
-		document.execCommand("copy");
-		document.body.removeChild(area);
-	});
+    let [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+    
+    chrome.cookies.getAll({"url":tab.url}, cookies => {
+        chrome.runtime.getPlatformInfo(platformInfo => {
+            var area = document.createElement('textarea');
+            area.style.position = 'absolute';
+            area.style.border = '0';
+            area.style.padding = '0';
+            area.style.margin = '0';
+            area.style.height = '1px';
+            area.style.top = '-10px';
+            
+            const data = {
+                cookies: cookies,
+                userAgent: navigator.userAgent
+            };
+            
+            area.innerText = JSON.stringify(data);
+            
+            document.body.appendChild(area, document.body.firstChild);
+            area.focus();
+            area.select();
+            document.execCommand("copy");
+            document.body.removeChild(area);
+        });
+    });
 })();
